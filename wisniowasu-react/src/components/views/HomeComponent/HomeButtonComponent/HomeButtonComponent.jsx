9dcd591ab4996238894cb29407d6661e25166e39
DvEyZ <szymonkwi@outlook.com>
2022-12-20 21:58:14 +0100

    zmieniono:       wisniowasu-react/src/components/views/HomeComponent/HomeButtonComponent/HomeButtonComponent.jsx

--- wisniowasu-react/src/components/views/HomeComponent/HomeButtonComponent/HomeButtonComponent.jsx
+++ wisniowasu-react/src/components/views/HomeComponent/HomeButtonComponent/HomeButtonComponent.jsx
@@ -5,11 +5,11 @@ class HomeButtonComponent extends React.Component
     render()
     {
         return (
-            <a class="button" href={this.props.href}>
+            <a class="button" href={ this.props.href }>
                 <div class="img_container">
                     <img data-src={"../../../../img/" + this.props.img } alt={this.props.alt} class="lazyload" />
                 </div>
-                <h3 class="button-text">{this.props.button_text}</h3>
+                <h3 class="button-text">{ this.props.button_text }</h3>
             </a>
         );
     }
