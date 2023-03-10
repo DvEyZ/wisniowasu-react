f7a81c4cab024514bba6f988ad7077684e9cf6e0
DvEyZ <szymonkwi@outlook.com>
2023-03-10 10:52:05 +0100

    usunięto:        src/img/icons/RRRRRRUDAAAA.png

--- src/js/mobileExt.js
+++
@@ -1,8 +0,0 @@
-export default function getIsMobile() {
-  var match = window.matchMedia || window.msMatchMedia;
-  if (match) {
-    var mq = match('(pointer:coarse)');
-    return mq.matches;
-  }
-  return false;
-}
