083994e33fa7a7a3ae2ea6bf34e90e339afd141b
DvEyZ <szymonkwi@outlook.com>
2023-03-10 10:42:18 +0100

    zmieniono:       wisniowasu-react/src/components/reusables/FooterComponent/FooterComponent.jsx

--- wisniowasu-react/src/components/reusables/FooterComponent/FooterComponent.jsx
+++ wisniowasu-react/src/components/reusables/FooterComponent/FooterComponent.jsx
@@ -19,7 +19,7 @@ export class FooterComponent extends React.Component   // DOKUMENTACJA
                 </div>
                 <p>Made by <a target="_blank" href="https://github.com/DvEyZ">Szymon Kwiręg</a> & <a href="https://github.com/resultfulness">Alicja Rosiak</a> | Source code on <a target="_blank" href="https://github.com/DvEyZ/homepage-wisniowasu">GitHub</a></p>
                 <p>Ikony wykorzystane z <a target="_blank" rel="noopener" href="https://icons8.com">Icons8</a></p>
-                <p>wersja {}</p>
+                <p>wersja 2.0.0</p>
             </footer>
         );
     }
